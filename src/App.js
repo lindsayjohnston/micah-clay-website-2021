80b384685f669cf444610cbcd90c249ab840f24d
Lindsay Johnston <lindsaykjohnston@outlook.com>
2021-10-12 10:21:48 -0500

    Added correctly sized logo for NavBar, set up NavBar design.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
-import logo from './assets/graphics/mc_skull_350x276.png';
 import NavBar from './modules/NavBar/NavBar.js';
+
+
 
 function App() {
   return (
--- src/modules/NavBar/NavBar.js
+++ src/modules/NavBar/NavBar.js
@@ -1,9 +1,17 @@
 import './NavBar.css';
-import logo from '../../assets/graphics/mc_skull_header_800x631.png';
+import logo from '../../assets/graphics/mc_skull_web_logo_106x78.png';
 
 const navBar = (props) => (
     <nav>
         <div className= "borderDiv">
+            <div className="logo"> 
+                <img src= {logo} alt= "skull-logo"/>
+            </div>
+            <div className="sectionLabel"> <h2>About </h2> </div>
+            <div className="sectionLabel"> <h2>About </h2> </div>
+            <div className="sectionLabel"> <h2>About </h2> </div>
+            <div className="sectionLabel"> <h2>About </h2> </div>
+           
 
         </div>
     </nav>
